cd2362dbd0389696cce26075a8cccabe34ce2c1d
SamK <samkdo771@gmail.com>
2018-05-01 15:12:27 +0200

    update pour connexion via mdp hashé.

--- logout.php
+++ logout.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
+
+//Suppression des variables de session et de la session
+$_SESSION = array();
+session_destroy();
 if(session_destroy())
 {
-header("Location: login.php");
+header("location: log_page.html");
 }
 ?>
