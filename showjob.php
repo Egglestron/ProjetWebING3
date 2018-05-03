d1d5d1cf7e127190db8d08b7ee5b25d5dd5916c6
DESKTOP-M2JB4NO\SamK <samkdo771@gmail.com>
2018-05-03 14:54:50 +0200

    affichage d'une annonce en particulier, petit probleme à corriger

--- showjob.php
+++ showjob.php
@@ -48,52 +48,55 @@
       </div>
     </nav>
 
-    <button class="btn btn-primary" onclick="window.location.href='addjob.html'" style="border-color: #000099; color: #000099; background-color: navbar-dark; text-align: center;" type="submit">Add a job offer</button>
+<?php
+include("config.php");
+$idp = isset($_POST["idpost"])?$_POST["idpost"]:null;
 
-    <?php
-    include("config.php");
-    //$id = $_SESSION["id"];
-
-    $requete = "SELECT jo.*, us.ID, us.FirstName, us.LastName, ob.Date_Post FROM joboffers jo, objectposts ob, users us";
-    $requete .= " WHERE jo.ID_Object = ob.ID AND ob.ID_User = us.ID ORDER BY ob.Date_Post DESC";
-
-    //echo $requete;
-
-    $req = mysqli_prepare($db, $requete);
-    mysqli_stmt_execute($req);
-
-    mysqli_stmt_store_result($req);
-
-    mysqli_stmt_bind_result($req, $col_IDObj, $col_JobLoc, $col_Company, $col_Title, $col_JobDescri, $col_Len, $col_Skills, $col_Area, $col_ID, $col_FirstName, $col_LastName, $col_DatePost);
-
-    while(mysqli_stmt_fetch($req)){
-
-   echo "<main role=\"main\" class=\"container col-sm-5\">";
-      echo "<div class=\"jumbotron float-center text-left\">";
-          echo "<h1 class=\"h3 mb-1\">Job Offer by $col_FirstName $col_LastName <br/> Job title : $col_Title</h1>";
-          echo "<button class=\"btn btn-primary\" onclick=\"window.location.href='showjob.php?idpost={$col_IDObj}'\" style=\"border-color: #000099; color: #000099; background-color: navbar-dark; text-align: center;\" type=\"submit\">Acces to the offer</button>";
-      echo "</div>";
-  echo "</main>";
-
-    }
-
-    ?>
+$requete = "SELECT jo.*, us.ID, us.FirstName, us.LastName, ob.Date_Post FROM joboffers jo, objectposts ob, users us";
+$requete .= " WHERE jo.ID_Object = ? AND jo.ID_Object = ob.ID AND ob.ID_User = us.ID";
 
 
-    <footer class="mastfoot mt-auto">
-      <div class="inner">
-        <p>LonkedOn by Arthur Prat, Maxime Michel and Sam Caddeo</p>
-      </div>
-    </footer>
+$req = mysqli_prepare($db, $requete);
+mysqli_stmt_bind_param($req, "i", $idp);
+mysqli_stmt_execute($req);
+
+mysqli_stmt_store_result($req);
+
+mysqli_stmt_bind_result($req, $col_IDObj, $col_JobLoc, $col_Company, $col_Title, $col_JobDescri, $col_Len, $col_Skills, $col_Area, $col_ID, $col_FirstName, $col_LastName, $col_DatePost);
+
+while(mysqli_stmt_fetch($req)){
+echo "<main role=\"main\" class=\"container col-sm-5\">";
+  echo "<div class=\"jumbotron float-center text-left\">";
+      echo "<h1 class=\"h3 mb-1\">Job Offer by $col_FirstName $col_LastName <br/> Job title : $col_Title</h1>";
+
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Offer n°$col_IDObj</p>";
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Job location : $col_JobLoc</p>";
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Company : $col_Company</p>";
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Description : $col_JobDescri</p>";
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Lenght : $col_Len months</p>";
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Skills required :  $col_Skills</p>";
+      echo "<p class=\"form-control mr-sm-2\" type=\"text\">Job area : $col_Area</p>";
+
+  echo "</div>";
+echo "</main>";
+}
+
+?>
+
+<footer class="mastfoot mt-auto">
+  <div class="inner">
+    <p>LonkedOn by Arthur Prat, Maxime Michel and Sam Caddeo</p>
+  </div>
+</footer>
 
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../../../dist/js/bootstrap.min.js"></script>
-  </body>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="../../../../assets/js/vendor/popper.min.js"></script>
+<script src="../../../../dist/js/bootstrap.min.js"></script>
+</body>
 </html>
